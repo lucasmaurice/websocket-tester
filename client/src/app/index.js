cdeee9a8df8d0b95afb7c0f35766d252f0cb3164
Sonic [Lucas Maurice] <sonic@djls.io>
2025-05-16 12:47:43 -0400

    Prettier refacto.

--- client/src/app/index.js
+++ client/src/app/index.js
@@ -1,28 +1,28 @@
-import React from 'react'
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import React from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 
-import Message from './components/message'
+import Message from "./components/message";
 
-import './styles.css'
+import "./styles.css";
 
-import * as actions from '../redux/actions'
+import * as actions from "../redux/actions";
 
-const mapStateToProps = (state) => ({ chat: state.chat })
-const mapDispatchToProps = (dispatch) => bindActionCreators(actions, dispatch)
+const mapStateToProps = (state) => ({ chat: state.chat });
+const mapDispatchToProps = (dispatch) => bindActionCreators(actions, dispatch);
 //add auto scroll to bottom
 @connect(mapStateToProps, mapDispatchToProps)
 class Home extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
-      message: ''
-    }
+      message: "",
+    };
   }
 
   componentWillMount() {
-    this.props.openConnection({ id: this.props.chat.id })
+    this.props.openConnection({ id: this.props.chat.id });
     this.scrollToBottom();
   }
 
@@ -31,66 +31,66 @@ class Home extends React.Component {
   }
 
   componentWillUnmount() {
-    this.props.chat.ws.close()
+    this.props.chat.ws.close();
   }
 
   onChange = (event) => {
-    this.setState({ [event.target.name]: event.target.value })
-  }
+    this.setState({ [event.target.name]: event.target.value });
+  };
 
   scrollToBottom = () => {
     if (this.textArea) {
       this.textArea.scrollTop = this.textArea.scrollHeight;
     }
-  }
+  };
 
   send = () => {
     if (this.state.message) {
-      this.props.chat.ws.send(JSON.stringify({
-        sender: this.props.chat.id,
-        message: this.state.message,
-        createdAt: Date.now()
-      }))
-      this.setState({ message: ''})
+      this.props.chat.ws.send(
+        JSON.stringify({
+          sender: this.props.chat.id,
+          message: this.state.message,
+          createdAt: Date.now(),
+        })
+      );
+      this.setState({ message: "" });
     }
-  }
+  };
 
   handleKeyPress = (event) => {
-    if (event.key == 'Enter') {
-      this.send()
+    if (event.key == "Enter") {
+      this.send();
     }
-  }
+  };
 
   render() {
-    const { id } = this.props.chat
+    const { id } = this.props.chat;
     return (
-      <div className='parent'>
-        <h2 style={{marginBottom: '0px'}}>You are user {id}</h2>
-        <h2 style={{marginTop: '5px'}}>You are now chatting with user {id % 2 === 0 ? id + 1 : id - 1}</h2>
-        <div className='chat-box'>
-          <div className='text-area' ref={element => { this.textArea = element }}>
-            {
-              this.props.chat.messages.map(m => 
-                <Message message={m} id={this.props.chat.id} />
-              )
-            }
+      <div className="parent">
+        <h2 style={{ marginBottom: "0px" }}>You are user {id}</h2>
+        <h2 style={{ marginTop: "5px" }}>You are now chatting with user {id % 2 === 0 ? id + 1 : id - 1}</h2>
+        <div className="chat-box">
+          <div
+            className="text-area"
+            ref={(element) => {
+              this.textArea = element;
+            }}
+          >
+            {this.props.chat.messages.map((m) => (
+              <Message message={m} id={this.props.chat.id} />
+            ))}
           </div>
-  
-          <div className='input-container'>
-            <input
-              className='input'
-              value={this.state.message}
-              name='message'
-              onChange={this.onChange}
-              placeholder='Say Hi!'
-              onKeyPress={this.handleKeyPress}
-            />
-            <a className='button' onClick={this.send}>Send!</a>
+
+          <div className="input-container">
+            <input className="input" value={this.state.message} name="message" onChange={this.onChange} placeholder="Say Hi!" onKeyPress={this.handleKeyPress} />
+            <a className="button" onClick={this.send}>
+              Send!
+            </a>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default Home
+export default Home;
--- client/webpack.production.config.js
+++ client/webpack.production.config.js
@@ -1,84 +1,76 @@
-const webpack = require('webpack')
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  context: __dirname + '/src',
+  context: __dirname + "/src",
   entry: {
-    app: './index.js',
-    vendor: [
-      'react',
-      'react-dom',
-      'redux',
-      'redux-thunk',
-    ]
+    app: "./index.js",
+    vendor: ["react", "react-dom", "redux", "redux-thunk"],
   },
   output: {
-    path: __dirname + '/dist',
-    filename: 'bundle.js'
+    path: __dirname + "/dist",
+    filename: "bundle.js",
   },
 
   resolve: {
-    extensions: ['.js', '.jsx', '.json', '.css']
+    extensions: [".js", ".jsx", ".json", ".css"],
   },
   module: {
     loaders: [
       { test: /\.jsx?$/, exclude: /node_modules/, use: ["babel-loader"] },
-      { test: /\.bundle\.js$/, loader: 'bundle-loader', options: { lazy: true } },
+      { test: /\.bundle\.js$/, loader: "bundle-loader", options: { lazy: true } },
       { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
       { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" },
-      { test: /\.css$/, loader: ExtractTextPlugin.extract({ use: 'css-loader' }) },
+      { test: /\.css$/, loader: ExtractTextPlugin.extract({ use: "css-loader" }) },
       { test: /\.png$/, loader: "url-loader?limit=100000" },
       { test: /\.ico$/, loader: "url-loader?limit=100000" },
-       { test: /\.(jpe?g)$/i, loaders: [
-            'file-loader?hash=sha512&digest=hex',
-            'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false'
-      ] },
-      { test: /manifest.json$/, loader: 'file-loader?name=manifest.json!web-app-manifest-loader' },
+      { test: /\.(jpe?g)$/i, loaders: ["file-loader?hash=sha512&digest=hex", "image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false"] },
+      { test: /manifest.json$/, loader: "file-loader?name=manifest.json!web-app-manifest-loader" },
       {
         test: /\.js$/,
         exclude: [/node_modules/],
-        loader: 'babel-loader',
+        loader: "babel-loader",
         query: {
           presets: ["env", "react", "stage-0"],
-          plugins: [
-              ["transform-decorators-legacy"],
-              ["transform-runtime"]
-            ]
-          }
+          plugins: [["transform-decorators-legacy"], ["transform-runtime"]],
+        },
       },
       {
         test: /\.scss$/,
-        use: [{
-            loader: "style-loader" 
-        }, {
-            loader: "css-loader" 
-        }, {
-            loader: "sass-loader" 
-        }]
-      }
-
-    ]
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+    ],
   },
   plugins: [
     new webpack.NoEmitOnErrorsPlugin(),
     new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      filename: 'vendor.bundle.js',
+      name: "vendor",
+      filename: "vendor.bundle.js",
     }),
-    new webpack.optimize.ModuleConcatenationPlugin(),    
+    new webpack.optimize.ModuleConcatenationPlugin(),
     new webpack.DefinePlugin({
       DEV: false,
     }),
     new webpack.LoaderOptionsPlugin({
-       minimize: true,
-       debug: false
-     }),
+      minimize: true,
+      debug: false,
+    }),
     new HtmlWebpackPlugin({
-      title: 'simple-boilerplate',
-      filename: 'index.html',
-      template: './index.html',
+      title: "simple-boilerplate",
+      filename: "index.html",
+      template: "./index.html",
       inject: true,
       minify: {
         collapseWhitespace: true,
@@ -87,10 +79,10 @@ module.exports = {
         minifyURLs: true,
         minifyJS: true,
         removeComments: true,
-        removeRedundantAttributes: true
-      }
+        removeRedundantAttributes: true,
+      },
     }),
     new webpack.optimize.AggressiveMergingPlugin(),
     new ExtractTextPlugin("styles.css"),
-  ]
-}
+  ],
+};
